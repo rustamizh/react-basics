aba01cada2f1551b6e9a3c3acb880ed0195577b8
Oleg Polyakov <olegpolyako@outlook.com>
2017-11-14 18:51:34 +0300

    End

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function App() {
-    return ();
+import Header from './components/Header';
+import Todo from './components/Todo';
+
+function App(props) {
+    return (
+        <main>
+            <Header title={props.title} />
+
+            <section className="todo-list">
+                {props.todos.map(todo =>
+                    <Todo key={todo.id} title={todo.title} completed={todo.completed} />
+                )}
+            </section>
+        </main>
+    );
 }
+
+App.propTypes = {
+    title: PropTypes.string,
+    todos: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        completed: PropTypes.bool.isRequired
+    })).isRequired
+};
+
+App.defaultProps = {
+    title: 'React Todo'
+};
 
 export default App;
