877fd55afd0b001b95525137170d9d199a8202bb
Rustam Khikmatullin <rustam.hikmatullin@piano.io>
2019-03-05 22:58:41 +0400

    Realized editing of tasks

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ class App extends React.Component {
         this.handleStatusChange = this.handleStatusChange.bind(this);
         this.handleDelete = this.handleDelete.bind(this);
         this.handleAdd = this.handleAdd.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
     }
 
     handleStatusChange(id) {
@@ -39,6 +40,17 @@ class App extends React.Component {
     nextId() {
         this._nextId = this._nextId || 4;
         return this._nextId++;
+    }
+
+    handleEdit(id, title) {
+        let todos = this.state.todos.map(todo => {
+            if (todo.id === id) todo.title = title;
+            return todo;
+        });
+
+        this.setState({
+            todos: todos
+        })
     }
 
     handleAdd(title) {
@@ -68,6 +80,7 @@ class App extends React.Component {
                             title={todo.title} 
                             completed={todo.completed} 
                             onDelete={this.handleDelete}
+                            onEdit={this.handleEdit}
                             onStatusChange={this.handleStatusChange}/>)
                     }
                 </section>
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,22 +3,62 @@ import PropTypes from 'prop-types';
 import Checkbox from './Checkbox';
 import Button from './Button';
 
-function Todo(props) {
+class Todo extends React.Component {
+    constructor(props) {
+        super(props);
 
-    return <div className={`todo ${props.completed ? 'completed' : ''}`}>
-                <Checkbox checked ={props.completed} onChange={() => props.onStatusChange(props.id)}/>
+        this.state = {
+            editing: false
+        };
 
-                <span className="todo-title">{props.title}</span>
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-                <Button className="delete icon" icon="delete" onClick={() => props.onDelete(props.id)}/>
-            </div>;
+    handleSubmit(event) {
+        event.preventDefault();
+
+        let title = this.refs.title.value;
+
+        this.props.onEdit(this.props.id, title);
+        this.setState({
+            editing: false
+        })
+    }
+
+    renderDisplay() {
+        return (
+            <div className={`todo ${this.props.completed ? 'completed' : ''}`}>
+                <Checkbox checked ={this.props.completed} onChange={() => this.props.onStatusChange(this.props.id)}/>
+
+                <span className="todo-title">{this.props.title}</span>
+                <Button className="edit icon" icon="edit" onClick={() => this.setState({editing: true})}/>
+                <Button className="delete icon" icon="delete" onClick={() => this.props.onDelete(this.props.id)}/>
+            </div>
+        )
+    }
+
+    renderForm() {
+        return (
+            <form className="todo-edit-form" onSubmit={this.handleSubmit}>
+                <input type="text" ref="title" defaultValue={this.props.title}/>
+                <Button className="save icon" icon="save" type="submit"/>
+            </form>
+        )
+    }
+
+    render() {
+        return this.state.editing ? this.renderForm() : this.renderDisplay()
+            
+    }
+    
 }
 
 Todo.propTypes = {
     title: PropTypes.string.isRequired,
     completed: PropTypes.bool.isRequired,
     onStatusChange: PropTypes.func.isRequired,
-    onDelete: PropTypes.func.isRequired
+    onDelete: PropTypes.func.isRequired,
+    onEdit: PropTypes.func.isRequired
 };
 
 export default Todo;
